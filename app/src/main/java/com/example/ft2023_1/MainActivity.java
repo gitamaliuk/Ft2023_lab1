207f9e32b9ff6b830d0a38e8a93ae7e217ad05bb
Gita <gita.maliukaite12@gmail.com>
2023-03-02 13:29:53 +0200

    Added changeText button and implemented logic

--- app/src/main/java/com/example/ft2023_1/MainActivity.java
+++ app/src/main/java/com/example/ft2023_1/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.ft2023_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.tvMain=findViewById(R.id.tvMain);
+    }
+
+    public void onBtnChangeMainText(View view) {
+        this.tvMain.setText("Text was changed!");
     }
 }
