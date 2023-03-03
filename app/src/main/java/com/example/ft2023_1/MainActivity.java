fd4a5917ded1a02138af76fab14eb632cffc18e4
Gita <gita.maliukaite12@gmail.com>
2023-03-03 15:35:34 +0200

    Revert "Added changeText button and implemented logic"
    This reverts commit 207f9e32b9ff6b830d0a38e8a93ae7e217ad05bb.

--- app/src/main/java/com/example/ft2023_1/MainActivity.java
+++ app/src/main/java/com/example/ft2023_1/MainActivity.java
@@ -4,21 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.tvMain=findViewById(R.id.tvMain);
-    }
-
-    public void onBtnChangeMainText(View view) {
-        this.tvMain.setText("Text was changed!");
     }
     public void OnBtnClickChangeColor(View view) {
         this.tvMain.setTextColor(Color.RED);
